26cdbf0ccc0f030bec6e17abf6859fca3485c270
Pooja <poojabharathi11@gmail.com>
2020-03-26 20:09:10 +0530

    samle

--- example.java
+++ example.java
@@ -3,12 +3,15 @@ class Example {
 
     public static void main(String args[]) {
 
-        int x = 45;
+        int x = 50;
         int y = 60;
 
         System.out.println(x);
         System.out.println(y);
+        System.out.println("");
+        
 
+        //example testing  master
         // swapping
         int temp = x;
         x = y;
