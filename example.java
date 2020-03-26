cf07598a86cf620ed48a29aa5d47f7d200a94efa
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2020-03-26 17:50:19 +0530

    initial commit

---
+++ example.java
@@ -0,0 +1,22 @@
+
+class Example {
+
+    public static void main(String args[]) {
+
+        int x = 50;
+        int y = 60;
+
+        System.out.println(x);
+        System.out.println(y);
+
+        // swapping
+        int temp = x;
+        x = y;
+        y = temp;
+
+        System.out.println(x);
+        System.out.println(y);
+
+    }
+
+}
